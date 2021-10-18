9824e3e3336bbe87db75694e72bdc5bf7e96a446
WT <27059876@qq.com>
2021-10-18 12:51:14 +0800

    补充json反序列化的代码

--- practice/testJson.cpp
+++ practice/testJson.cpp
@@ -70,17 +70,52 @@ string simulationRecv2(){
     //js["msg"] = {{"zhang san", "hello world"}, {"liu shuo", "hello China"}};
     return js.dump();
 }
+string simulationRecv3(){
+    json js;
+
+    vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(5);
+
+    js["list"] = vec;
+
+    map<int, string> m;
+    m.insert({1,"黄山"});
+    m.insert({2,"华山"});
+    m.insert({3,"泰山"});
+
+    js["path"] = m;
+
+    return js.dump();
+}
 int main(){
     //string recvBuf = simulationRecv1();
-    string recvBuf = simulationRecv2();
+
+    //string recvBuf = simulationRecv2();
+
+    string recvBuf = simulationRecv3();
 
     json jsbuf = json::parse(recvBuf);
+
     //cout<<jsbuf["msg_type"]<<endl;
     //cout<<jsbuf["from"]<<endl;
     //cout<<jsbuf["to"]<<endl;
     //cout<<jsbuf["msg"]<<endl;
-    cout<<jsbuf["id"]<<endl;
-    auto arr = jsbuf["id"];
-    cout<<arr[2]<<endl;
+
+    //cout<<jsbuf["id"]<<endl;
+    //auto arr = jsbuf["id"];
+    //cout<<arr[2]<<endl;
+
+    vector<int> vec = jsbuf["list"];
+    for(auto &v:vec){
+        cout<<v<<' ';
+    }
+    cout<<endl;
+    map<int, string> mymap = jsbuf["path"];
+    for(auto &p:mymap){
+        cout<<p.first<<' '<<p.second<<endl;
+    }
+    cout<<endl;
     return 0;
 }
